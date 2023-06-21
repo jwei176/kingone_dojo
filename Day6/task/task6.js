d8c333c56854176b95d0ae90323bd7a14f2ae5b2
tan-jun-wei <tan-jun-wei@cybozu.co.jp>
2023-06-21 13:44:42 +0900

    task6

---
+++ Day6/task/task6.js
@@ -0,0 +1,20 @@
+(() => {
+    'use strict';
+    kintone.events.on('app.record.create.show',(event) => {
+        const list = [];
+        const Action = ["あくなき探求", "不屈の心体", "理想への共感", "心を動かす", "知識を増やす", "公明正大"];
+        console.log(event);
+        
+        //６つのnullフィールドを作成し、Action5+1のコンテントを入れる
+        for(let i=0; i<6; i++) {
+            list.push(structuredClone(event.record.Table.value[0]));
+            list[i].value.Action5.value = Action[i];
+        };
+
+        //listをtableに代入する
+        event.record.Table.value = list;
+       
+        return event;
+    
+    });
+})();
