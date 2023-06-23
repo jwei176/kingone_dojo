7491627ce97744df1f31ed70c44caf8abdf53f6a
tan-jun-wei <tan-jun-wei@cybozu.co.jp>
2023-06-23 13:44:08 +0900

    task7

--- task7.js
+++ task7.js
@@ -1,25 +1,21 @@
 (() => {
-	'use strict';
-    
-    
-  kintone.events.on('app.record.create.show', async (event) => {
-    // console.log(event);
-    let date = dayjs(event.record.日付.value).format('YYYYMMDD');
-    let dropdown = event.record.サイボウズ製品.value;
+  'use strict';
+  const fields = ['app.record.create.change.日付', 'app.record.create.change.サイボウズ製品', 'app.record.create.change.管理番号'];
+
+  kintone.events.on(['app.record.create.show'], async (event) => {
+    console.log(event);
     let product = '';
     let manageNO = event.record.管理番号.value;
+    let date = dayjs(event.record.日付.value).format('YYYYMMDD');
+    let dropdown = event.record.サイボウズ製品.value;
+    event.record.重複禁止項目_文字列.value = `${date}-${product}-${manageNO}`;
 
-    //日付のフィールド
-    kintone.events.on('app.record.create.change.日付', (event1) => {
-        date = dayjs(event1.record.日付.value).format('YYYYMMDD');
-        // newdate = dayjs(date).format('YYYYMMDD');
-        event1.record.重複禁止項目_文字列.value = `${date}-${product}-${manageNO}`;
-        return event1;
-    });
 
-    //サイボウズ商品のフィールド
-    kintone.events.on('app.record.create.change.サイボウズ製品', (event2) => {
-      dropdown = event2.record.サイボウズ製品.value;
+    await kintone.events.on(fields, (event) => {
+
+      date = dayjs(event.record.日付.value).format('YYYYMMDD');
+
+      dropdown = event.record.サイボウズ製品.value;
       if (dropdown === 'kintone') {
         product = 'KN';
       } else if (dropdown === 'Garoon') {
@@ -28,20 +24,13 @@
         product = 'OF';
       } else if (dropdown === 'Mailwise') {
         product = 'MW';
-      };
-      event2.record.重複禁止項目_文字列.value = `${date}-${product}-${manageNO}`;
-      return event2;
+      }
+
+      manageNO = event.record.管理番号.value;
+      event.record.重複禁止項目_文字列.value = `${date}-${product}-${manageNO}`;
+      return event;
     });
 
-    //管理番号のフィールド
-    kintone.events.on('app.record.create.change.管理番号', (event3) => {
-      manageNO = event3.record.管理番号.value;
-      event3.record.重複禁止項目_文字列.value = `${date}-${product}-${manageNO}`;
-      return event3;
-    });
-
-    //update 重複禁止項目_文字列
-    event.record.重複禁止項目_文字列.value = `${date}-${product}-${manageNO}`;
     event.record.重複禁止項目_文字列.disabled = true;
     return event;
   });
