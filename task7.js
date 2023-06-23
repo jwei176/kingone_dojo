6212096f3467b69b4ac1c31924d9183ac3402d9a
tan-jun-wei <tan-jun-wei@cybozu.co.jp>
2023-06-23 15:00:29 +0900

    task7

--- task7.js
+++ task7.js
@@ -2,34 +2,28 @@
   'use strict';
   const fields = ['app.record.create.change.日付', 'app.record.create.change.サイボウズ製品', 'app.record.create.change.管理番号'];
 
-  kintone.events.on(['app.record.create.show'], async (event) => {
+  kintone.events.on(['app.record.create.show',fields], (event) => {
     console.log(event);
     let product = '';
     let manageNO = event.record.管理番号.value;
     let date = dayjs(event.record.日付.value).format('YYYYMMDD');
     let dropdown = event.record.サイボウズ製品.value;
+
+    date = dayjs(event.record.日付.value).format('YYYYMMDD');
+
+    dropdown = event.record.サイボウズ製品.value;
+    if (dropdown === 'kintone') {
+      product = 'KN';
+    } else if (dropdown === 'Garoon') {
+      product = 'GR';
+    } else if (dropdown === 'サイボウズ Office') {
+      product = 'OF';
+    } else if (dropdown === 'Mailwise') {
+      product = 'MW';
+    }
+
+    manageNO = event.record.管理番号.value;
     event.record.重複禁止項目_文字列.value = `${date}-${product}-${manageNO}`;
-
-
-    await kintone.events.on(fields, (event) => {
-
-      date = dayjs(event.record.日付.value).format('YYYYMMDD');
-
-      dropdown = event.record.サイボウズ製品.value;
-      if (dropdown === 'kintone') {
-        product = 'KN';
-      } else if (dropdown === 'Garoon') {
-        product = 'GR';
-      } else if (dropdown === 'サイボウズ Office') {
-        product = 'OF';
-      } else if (dropdown === 'Mailwise') {
-        product = 'MW';
-      }
-
-      manageNO = event.record.管理番号.value;
-      event.record.重複禁止項目_文字列.value = `${date}-${product}-${manageNO}`;
-      return event;
-    });
 
     event.record.重複禁止項目_文字列.disabled = true;
     return event;
