a3c593e2de7f6f34f9e66ed990ab0c9ce33a16ed
tan-jun-wei <tan-jun-wei@cybozu.co.jp>
2023-06-26 18:34:07 +0900

    task9

--- task9.js
+++ task9.js
@@ -1,6 +1,6 @@
 (() => {
   'use strict';
- 
+
   kintone.events.on('app.record.create.submit', async (event) => {
     const context = event.record.重複禁止項目.value;
     const ID = kintone.app.getId();
@@ -8,30 +8,43 @@
       app: ID,
     };
 
-    //RESTAPIでレコードの情報を収得する
     const resp = await kintone.api(kintone.api.url('/k/v1/records.json'), 'GET', params);
     const record_info = resp.records;
-    // console.log(resp);
     const record_list = [];
     Object.values(record_info).forEach((ele) => {
-      // console.log(ele.重複禁止項目.value);
       record_list.push(ele.重複禁止項目.value);
     });
     console.log(record_list);
 
     if (record_list.includes(context) === true) {
-      //重複してる場合
       const answer = window.confirm('レコードが重複しています。このまま保存しますか？');
-      if (answer) {
-        //confirmを押すと、eventをreturnし、保存が実行されます。
-        return event;
-      } else {
-        //キャンセルを押すと、保存の処理はしません
+      if (!answer) {
         return false;
       }
-    } else {
-      //重複していない場合
-      return event;
     }
+    return event;
+  });
+  kintone.events.on('app.record.edit.submit', async (event) => {
+    const context = event.record.重複禁止項目.value;
+    const ID = kintone.app.getId();
+    const params = {
+      app: ID,
+    };
+
+    const resp = await kintone.api(kintone.api.url('/k/v1/records.json'), 'GET', params);
+    const record_info = resp.records;
+    const record_list = [];
+    Object.values(record_info).forEach((ele) => {
+      record_list.push(ele.重複禁止項目.value);
+    });
+    console.log(record_list);
+
+    if (record_list.includes(context) === true) {
+      const answer = window.confirm('レコードが重複しています。このまま保存しますか？');
+      if (!answer) {
+        return false;
+      }
+    }
+    return event;
   });
 })();
